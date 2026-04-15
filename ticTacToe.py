d5b09e7bcf25291d859b1f30c9f2916906f87c2c
nranalli2029 <ner5282@psu.edu>
2026-04-15 16:16:30 -0400

    check_winner() and is_draw()

--- ticTacToe.py
+++ ticTacToe.py
@@ -3,3 +3,22 @@ class TicTacToe():
         self.board = [["-", "-", "-"], ["-", "-", "-"],["-", "-", "-"]]
         self.current = "O"
         self.gameOver = False
+
+    def check_winner(self):
+        for i in range(3):
+            if self.board[i][0] == self.board[i][1] == self.board[i][2] == self.current:
+                return True
+            if self.board[0][i] == self.board[1][i] == self.board[2][i] == self.current:
+                return True
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] == self.current:
+            return True
+        if self.board[0][2] == self.board[1][1] == self.board[2][0] == self.current:
+            return True
+        return False
+        
+    def is_draw(self):
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                if self.board[i][j] == "-":
+                    return False
+        return True
